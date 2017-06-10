89b35f88025696207c90c93f3f727771db1623a9
AntoineBenard76 <antoine.benard@uha.fr>
2017-06-10 14:51:44 +0200

    Nolwenn en +

--- apropos.php
+++ apropos.php
@@ -12,6 +12,7 @@
             <p><b>L'EQUIPE : </b></p>
             <p><img src="img/equipe/antoine.jpg" width=100 height=100 alt="Bug Antoine"/> Chef du Projet ensicafé, étudiant dynamique et motivé qui a fait de son mieux pour booster les troupes ! :D</p>
             <p><img src="img/equipe/olivier.jpg" width=100 height=100 alt="Bug Olivier"/> Roi du design, cet homme a rendu non-radioactif le style de tout le projet !</p>
+            <p><img src="img/equipe/nolwenn.jpg" width=100 height=160 alt="Bug Nolwenn"/> Travailleuse acharnée, elle est également la touche féminine de ce projet !</p>
             <p><b> L'ORIGINE DU PROJET :</b></p>
             <p><a href="http://www.ensisa.uha.fr/"><img src="img/ensisa.jpg" width=180 height=80 alt="Bug Ensisa"/></a> Ce projet nous a été proposé par Jonathan WEBER et de manière plus générale par l'ENSISA, école d'ingénieurs à Mulhouse.</p>
             <p><b> REMERCIEMENTS : </b></p>
