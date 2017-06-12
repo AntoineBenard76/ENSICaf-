6d76e99bf9d698d056ad0b77a0141939b3a4fb28
Lumi <olivier.tinh@gmail.com>
2017-06-12 23:00:28 +0200

    contenu sidebar.php

--- php/sidebar.php
+++ php/sidebar.php
@@ -23,11 +23,13 @@
                     <li><a href="#"><span class="glyphicon glyphicon-bullhorn"></span>Annonces</a></li>
                     <li><a href="#"><span class="glyphicon glyphicon-user"></span>Cercles</a></li>
                     <hr class="sidebar-divider">
-                    <li><a href="http://www.uha.fr/"><span class="glyphicon glyphicon-education"></span>Université de Haute-Alsace</a></li>
-                    <li><a href="http://www.ensisa.uha.fr/"><span class="glyphicon glyphicon-education"></span>ENSISA</a></li>
-                    <li><a href="https://www.e-services.uha.fr/"><span class="glyphicon glyphicon-globe"></span>e-services</a></li>
-                    <li><a href="http://www.emploisdutemps.fr/edt/PIWvD"><span class="glyphicon glyphicon-calendar"></span>Emploi du temps (IR/AS)</a></li>
                     <li><a href="http://www.emploisdutemps.fr/edt/LNzcL"><span class="glyphicon glyphicon-calendar"></span>Emploi du temps (Text/Méca)</a></li>
+                    <li><a href="http://www.emploisdutemps.fr/edt/PIWvD"><span class="glyphicon glyphicon-calendar"></span>Emploi du temps (IR/AS)</a></li>
+                    <li><a href="http://www.emploisdutemps.fr/edt/dUtVu"><span class="glyphicon glyphicon-calendar"></span>Emploi du temps (FIP)</a></li>
+                    <hr class="sidebar-divider">
+                    <li><a href="http://www.uha.fr/"><span class="glyphicon glyphicon-education"></span>Université de Haute-Alsace</a></li>
+                    <li><a href="https://www.e-services.uha.fr/"><span class="glyphicon glyphicon-globe"></span>e-services</a></li>
+                    <li><a href="http://www.ensisa.uha.fr/"><span class="glyphicon glyphicon-education"></span>ENSISA</a></li>
                     <hr class="sidebar-divider">
                     <li><a href="apropos.php"><span class="glyphicon glyphicon-question-sign"></span>A propos</a></li>
                 </ul>
