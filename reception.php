d28dabc73fdd80ec3feb5a687594a7ac7c633581
Lumi <olivier.tinh@gmail.com>
2017-06-12 13:39:08 +0200

    bouton "marqué comme lus"

--- reception.php
+++ reception.php
@@ -21,6 +21,7 @@
     </div>
     <!-- /#titre -->
     
+    <!-- Panel reception -->
     <div class="jumbotron custom-jumbotron panel-reception">
 
         <!-- Navigation -->
@@ -45,6 +46,16 @@
                     <li>
                         <p class="navbar-btn">
                             <a href="envoi.php" class="btn btn-info"><span class="glyphicon glyphicon-envelope"></span>Envoyer un message</a>
+                        </p>
+                    </li>
+                    <li>
+                        <p class="navbar-btn">
+                            <form method="POST" action="lu.php">
+                                <button type="submit" class="btn btn-warning">
+                                    <span class="glyphicon glyphicon-eye-open"></span>
+                                    Marquer comme lus
+                                </button>
+                            </form>
                         </p>
                     </li>
                 </ul>
@@ -87,14 +98,8 @@
                 <?php } ?>
             </div>
         </div>
-    
-    <div align="center">
-        <form method="POST" action="lu.php">
-            <input type="submit" value="Marquer tous les messages comme lus"/>
-        </form>    
     </div>
-    </div>
-
+    <!-- /#panel-reception -->
 </div>
 
 <!-- Contenu principal -->
