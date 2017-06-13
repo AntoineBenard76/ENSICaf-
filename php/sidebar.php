0f7138c56af8e1b0d1585c89da938a63ed2e6ab5
Lumi <olivier.tinh@gmail.com>
2017-06-13 19:36:08 +0200

    W3C

--- php/sidebar.php
+++ php/sidebar.php
@@ -21,15 +21,15 @@
                     <li><a href="club.php"><span class="glyphicon glyphicon-pawn"></span>Clubs</a></li>
                     <li><a href="reception.php"><span class="glyphicon glyphicon-envelope"></span>Boîte de réception</a></li>
                     <li><a href="annonce.php"><span class="glyphicon glyphicon-bullhorn"></span>Annonces</a></li>
-                    <hr class="sidebar-divider">
+                    <li><hr class="sidebar-divider"></li>
                     <li><a href="http://www.emploisdutemps.fr/edt/LNzcL"><span class="glyphicon glyphicon-calendar"></span>Emploi du temps (Text/Méca)</a></li>
                     <li><a href="http://www.emploisdutemps.fr/edt/PIWvD"><span class="glyphicon glyphicon-calendar"></span>Emploi du temps (IR/AS)</a></li>
                     <li><a href="http://www.emploisdutemps.fr/edt/dUtVu"><span class="glyphicon glyphicon-calendar"></span>Emploi du temps (FIP)</a></li>
-                    <hr class="sidebar-divider">
+                    <li><hr class="sidebar-divider"></li>
                     <li><a href="http://www.uha.fr/"><span class="glyphicon glyphicon-education"></span>Université de Haute-Alsace</a></li>
                     <li><a href="https://www.e-services.uha.fr/"><span class="glyphicon glyphicon-globe"></span>e-services</a></li>
                     <li><a href="http://www.ensisa.uha.fr/"><span class="glyphicon glyphicon-education"></span>ENSISA</a></li>
-                    <hr class="sidebar-divider">
+                    <li><hr class="sidebar-divider"></li>
                     <li><a href="apropos.php"><span class="glyphicon glyphicon-question-sign"></span>A propos</a></li>
                 </ul>
             </div>
